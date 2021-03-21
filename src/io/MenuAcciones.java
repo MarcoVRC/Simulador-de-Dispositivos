af2aeca2d917b54265d40fc471ffac44ce4fb44d
MarcoVRC <mrac193@gmail.com>
2021-03-21 16:47:37 -0600

    Se agregaron los menus para realizar acciones con los dispositivos y posteriormente ver esas acciones.

--- src/io/MenuAcciones.java
+++ src/io/MenuAcciones.java
@@ -22,44 +22,53 @@ public class MenuAcciones {
 	public static MenuAcciones getInstancia() {
 	return Instancia;
 	}
-	
+        /**
+        * 
+        * MENU PRINCIPAL DE ACCIONES CON DISPOSITIVOS
+        */
 	public void elegirAccion(){
-         try{   
-	String opcion = "";
-	do{
-	System.out.println(" "); 
-	System.out.println("       "+"-----ACCIONES CON DISPOSITIVOS-----");
-	System.out.println("");
-	System.out.println("1. Computadora Portatil ");
-	System.out.println("2. Tablet");
-        System.out.println("3. SmartWatch");
-        System.out.println("4. SmartPhone");
-        System.out.println("5. Auriculares Inalambricos");
-	System.out.println("   "+"Escriba Salir Si Lo Desea");
-	
-	opcion=dato.readLine();
-	
-	switch(opcion){
-		case "1":
-               elegirAccionPortatil();
-                    break;
-                
-                case "2":
-                
-                    break;
-                case"3":
+            try{
+                String opcion = "";
+                do{
+                    System.out.println(" "); 
+                    System.out.println("       "+"-----ACCIONES CON DISPOSITIVOS-----");
+                    System.out.println("");
+                    System.out.println("1. Computadora Portatil ");
+                    System.out.println("2. Tablet");
+                    System.out.println("3. SmartWatch");
+                    System.out.println("4. SmartPhone");
+                    System.out.println("5. Auriculares Inalambricos");
+                    System.out.println("6. Acciones hechas por dispositivos ");
+                    System.out.println("   "+"Escriba Salir Si Lo Desea");
+                    opcion=dato.readLine();
+                    switch(opcion){
+                        case "1":
+                            elegirAccionPortatil();
+                            break;
+                        case "2":
+                        elegirAccionTablet();        
+                            break;
+                        case"3":
+                        elegirAccionReloj();    
+                            break;
+                        case"4":
+                            elegirAccionPhone();     
+                            break;
+                        case"5":
                     
-                    break;
-                case"4":
-                elegirAccionPhone();     
-                    break;
-                case"5":
-                    
-                    break;
-		}
-		}while (!opcion.toLowerCase().equals("salir"));
-        }catch(IOException e){}
-	}
+                            break;
+                        case"6":
+                            elegirAccionMostrar();
+                            break;
+                    }
+                }while (!opcion.toLowerCase().equals("salir"));
+            }catch(IOException e){}
+        }
+        
+        /**
+        * 
+        * MENU DE PORTATILES PARA REALIZAR ACCIONES CON LOS DISPOSITIVOS PORTATILES.
+        */
         
         public void elegirAccionPortatil(){
          try{   
@@ -73,6 +82,7 @@ public class MenuAcciones {
         System.out.println("3. Pegar Texto");
         System.out.println("4. Compartir Documentos");
         System.out.println("5. Reproducir Musica");
+        
 	System.out.println("   "+"Escriba Salir Si Lo Desea");
 	
 	opcion=dato.readLine();
@@ -83,13 +93,13 @@ public class MenuAcciones {
                     break;
                 
                 case "2":
-                
+                ManejoComputadoraPortatil.getInstancia().copiarTextoPortatil();
                     break;
                 case"3":
-                    
+                ManejoComputadoraPortatil.getInstancia().pegarTextoPortatil();    
                     break;
                 case"4":
-                    ManejoSmartPhone.getInstancia().capturarFotoPhone();
+                   
                     break;
                 case"5":
                     
@@ -97,7 +107,100 @@ public class MenuAcciones {
 		}
 		}while (!opcion.toLowerCase().equals("salir"));
         }catch(IOException e){}
-	}
+        }
+        
+        /**
+        * 
+        * MENU DE TABLETS PARA REALIZAR ACCIONES CON TABLETS.
+        */
+        
+        public void elegirAccionTablet(){
+         try{   
+	String opcion = "";
+	do{
+	System.out.println(" "); 
+	System.out.println("       "+"-----ACCIONES CON DISPOSITIVOS-----");
+	System.out.println("");
+	System.out.println("1. Tomar Fotografia ");
+	System.out.println("2. Copiar Texto");
+        System.out.println("3. Pegar Texto");
+        System.out.println("4. Compartir Documentos");
+        System.out.println("5. Reproducir Musica");
+        
+	System.out.println("   "+"Escriba Salir Si Lo Desea");
+	
+	opcion=dato.readLine();
+	
+	switch(opcion){
+		case "1":
+                    ManejoTablet.getInstancia().capturarFotoTablet();
+                    break;
+                
+                case "2":
+                    ManejoTablet.getInstancia().copiarTextoTablet();
+                    break;
+                case"3":
+                    ManejoTablet.getInstancia().pegarTextoTablet();
+                    break;
+                case"4":
+                   
+                    break;
+                case"5":
+                    
+                    break;
+		}
+		}while (!opcion.toLowerCase().equals("salir"));
+        }catch(IOException e){}
+	} 
+        
+        /**
+        * 
+        * MENU DE SMARTWATCHS PARA REALIZAR ACCIONES CON SMARTWATCHS.
+        */
+        
+        public void elegirAccionReloj(){
+         try{   
+	String opcion = "";
+	do{
+	System.out.println(" "); 
+	System.out.println("       "+"-----ACCIONES CON DISPOSITIVOS-----");
+	System.out.println("");
+	System.out.println("1. Tomar Fotografia ");
+	System.out.println("2. Copiar Texto");
+        System.out.println("3. Pegar Texto");
+        System.out.println("4. Compartir Documentos");
+        System.out.println("5. Reproducir Musica");
+        
+	System.out.println("   "+"Escriba Salir Si Lo Desea");
+	
+	opcion=dato.readLine();
+	
+	switch(opcion){
+		case "1":
+                    ManejoSmartWatch.getInstancia().capturarFotoReloj();
+                    break;
+                
+                case "2":
+                
+                    break;
+                case"3":
+                    
+                    break;
+                case"4":
+                   
+                    break;
+                case"5":
+                    
+                    break;
+		}
+		}while (!opcion.toLowerCase().equals("salir"));
+        }catch(IOException e){}
+	} 
+        
+        /**
+        * 
+        * MENU DE SMARTPHONES PARA REALIZAR ACCIONES CON LOS DISPOSITIVOS SMARTPHONE.
+        */
         
         public void elegirAccionPhone(){
          try{   
@@ -117,13 +220,13 @@ public class MenuAcciones {
 	
 	switch(opcion){
 		case "1":
-                   ManejoSmartPhone.getInstancia().capturarFotoPhone();
+                ManejoSmartPhone.getInstancia().capturarFotoPhone();
                     break;
-                
                 case "2":
-                
+                ManejoSmartPhone.getInstancia().copiarTextoPhone();
                     break;
                 case"3":
+                ManejoSmartPhone.getInstancia().pegarTextoPhone();
                     
                     break;
                 case"4":
@@ -136,4 +239,199 @@ public class MenuAcciones {
 		}while (!opcion.toLowerCase().equals("salir"));
         }catch(IOException e){}
 	}
+        
+        /**
+        * 
+        * MENU DE DISPOSITIVOS MUESTRA LAS ACCIONES REALIZADAS POR LOS DISPOSITIVOS.
+        */
+        
+        public void elegirAccionMostrar(){
+            try{
+                String opcion = "";
+                do{
+                    System.out.println(" "); 
+                    System.out.println("       "+"-----ACCIONES HECHAS POR DISPOSITIVOS-----");
+                    System.out.println("");
+                    System.out.println("1. Computadora Portatil ");
+                    System.out.println("2. Tablet");
+                    System.out.println("3. SmartWatch");
+                    System.out.println("4. SmartPhone");
+                    System.out.println("5. Auriculares Inalambricos");
+                    System.out.println("   "+"Escriba Salir Si Lo Desea");
+                    opcion=dato.readLine();
+                    switch(opcion){
+                        case "1":
+                            mostrarAccionPortatil();
+                            break;
+                        case "2":
+                        mostrarAccionTablet();    
+                            break;
+                        case"3":
+                        mostrarAccionReloj();    
+                            break;
+                        case"4":
+                        mostrarAccionPhone();    
+                            break;
+                        case"5":
+                    
+                            break;
+                    }
+                }while (!opcion.toLowerCase().equals("salir"));
+            }catch(IOException e){}
+        }
+        
+        /**
+        * 
+        * "MENU DE MOSTRAR ACCIONES DE PORTATILES" PARA MOSTRAR LAS ACCIONES REALIZADAS POR LOS DISPOSITIVOS PORTATILES.
+        */
+        
+        public void mostrarAccionPortatil(){
+        try{
+                String opcion = "";
+                do{
+                    System.out.println(" "); 
+                    System.out.println("       "+"-----ACCIONES HECHAS POR DISPOSITIVOS-----");
+                    System.out.println("");
+                    System.out.println("1. Galeria ");
+                    System.out.println("2. Textos Copiados");
+                    System.out.println("3. Pegar Texto");
+                    System.out.println("4. Documentos Compartidos");
+                    System.out.println("5. ");
+                    System.out.println("   "+"Escriba Salir Si Lo Desea");
+                    opcion=dato.readLine();
+                    switch(opcion){
+                        case "1":
+                            ManejoComputadoraPortatil.getInstancia().mostrarFotoPortatil();
+                            break;
+                        case "2":
+                            break;
+                        case"3":
+                    
+                            break;
+                        case"4":
+                
+                            break;
+                        case"5":
+                    
+                            break;
+                    }
+                }while (!opcion.toLowerCase().equals("salir"));
+            }catch(IOException e){}
+	}
+        
+        /**
+        * 
+        * "MENU DE MOSTRAR ACCIONES DE TABLETS" PARA MOSTRAR LAS ACCIONES REALIZADAS POR LOS DISPOSITIVOS TABLETS.
+        */
+        public void mostrarAccionTablet(){
+        try{
+                String opcion = "";
+                do{
+                    System.out.println(" "); 
+                    System.out.println("       "+"-----ACCIONES HECHAS POR DISPOSITIVOS-----");
+                    System.out.println("");
+                    System.out.println("1. Galeria ");
+                    System.out.println("2. Textos Copiados");
+                    System.out.println("3. Pegar Texto");
+                    System.out.println("4. Documentos Compartidos");
+                    System.out.println("5. ");
+                    System.out.println("   "+"Escriba Salir Si Lo Desea");
+                    opcion=dato.readLine();
+                    switch(opcion){
+                        case "1":
+                            ManejoTablet.getInstancia().mostrarFotoTablet();
+                            break;
+                        case "2":
+                            break;
+                        case"3":
+                    
+                            break;
+                        case"4":
+                
+                            break;
+                        case"5":
+                    
+                            break;
+                    }
+                }while (!opcion.toLowerCase().equals("salir"));
+            }catch(IOException e){}
+	}
+        
+        /**
+        * 
+        * "MENU DE MOSTRAR ACCIONES DE SMARTWATCHS" PARA MOSTRAR LAS ACCIONES REALIZADAS POR LOS DISPOSITIVOS SMARTWATCHS.
+        */
+        
+        public void mostrarAccionReloj(){
+        try{
+                String opcion = "";
+                do{
+                    System.out.println(" "); 
+                    System.out.println("       "+"-----ACCIONES HECHAS POR DISPOSITIVOS-----");
+                    System.out.println("");
+                    System.out.println("1. Galeria ");
+                    System.out.println("2. Textos Copiados");
+                    System.out.println("3. Pegar Texto");
+                    System.out.println("4. Documentos Compartidos");
+                    System.out.println("5. ");
+                    System.out.println("   "+"Escriba Salir Si Lo Desea");
+                    opcion=dato.readLine();
+                    switch(opcion){
+                        case "1":
+                            ManejoSmartWatch.getInstancia().mostrarFotoReloj();
+                            break;
+                        case "2":
+                            break;
+                        case"3":
+                    
+                            break;
+                        case"4":
+                
+                            break;
+                        case"5":
+                    
+                            break;
+                    }
+                }while (!opcion.toLowerCase().equals("salir"));
+            }catch(IOException e){}
+	}
+        
+        /**
+        * 
+        * "MENU DE MOSTRAR ACCIONES DE SMARTWATCHS" PARA MOSTRAR LAS ACCIONES REALIZADAS POR LOS DISPOSITIVOS SMARTWATCHS.
+        */
+        
+        public void mostrarAccionPhone(){
+            try{
+                String opcion = "";
+                do{
+                    System.out.println(" "); 
+                    System.out.println("       "+"-----ACCIONES HECHAS POR DISPOSITIVOS-----");
+                    System.out.println("");
+                    System.out.println("1. Galeria ");
+                    System.out.println("2. Textos Copiados");
+                    System.out.println("3. Pegar Texto");
+                    System.out.println("4. Documentos Compartidos");
+                    System.out.println("5. ");
+                    System.out.println("   "+"Escriba Salir Si Lo Desea");
+                    opcion=dato.readLine();
+                    switch(opcion){
+                        case "1":
+                            ManejoSmartPhone.getInstancia().mostrarFotoPhone();
+                            break;
+                        case "2":
+                            break;
+                        case"3":
+                    
+                            break;
+                        case"4":
+                
+                            break;
+                        case"5":
+                    
+                            break;
+                    }
+                }while (!opcion.toLowerCase().equals("salir"));
+            }catch(IOException e){}
+        }
 }
