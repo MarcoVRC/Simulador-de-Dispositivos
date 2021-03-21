3fdb73a43abc590b38bb321d24a1802361e7a532
MarcoVRC <mrac193@gmail.com>
2021-03-21 16:43:50 -0600

    Creacion del bean Fotos, para almacenar fotos de los dispositivos 1,2,3,4.

---
+++ src/bean/Foto.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bean;
+
+/**
+ *
+ * @author RC
+ */
+public class Foto {
+    private String nombreFoto;
+    private String tipoDispositivo;
+    private String nombreDispositivo;
+    private int idDispositivo;
+    
+    public String getNombreFoto() {
+        return nombreFoto;
+    }
+
+    public void setNombreFoto(String nombreFoto) {
+        this.nombreFoto = nombreFoto;
+    }
+
+    public String getTipoDispositivo() {
+        return tipoDispositivo;
+    }
+
+    public void setTipoDispositivo(String tipoDispositivo) {
+        this.tipoDispositivo = tipoDispositivo;
+    }
+
+    public String getNombreDispositivo() {
+        return nombreDispositivo;
+    }
+
+    public void setNombreDispositivo(String nombreDispositivo) {
+        this.nombreDispositivo = nombreDispositivo;
+    }
+
+    public int getIdDispositivo() {
+        return idDispositivo;
+    }
+
+    public void setIdDispositivo(int idDispositivo) {
+        this.idDispositivo = idDispositivo;
+    }
+    
+}
