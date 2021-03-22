d49547d20c33476ccc84d89549651c4b86acd099
MarcoVRC <mrac193@gmail.com>
2021-03-21 21:23:18 -0600

    Se agrego la funcion compartir documentos y mostrarlista.

--- src/procesador/Procesador.java
+++ src/procesador/Procesador.java
@@ -39,7 +39,6 @@ public void procesarAccion(String tipoDispositivo)
     switch(dispositivo.toUpperCase().trim())
     {
         case "COMPUTADORA PORTATIL":
-           // System.out.println(tipoDispositivo);
            cargarPortatil(parts);
             break;
         case "SMARTPHONE":
@@ -60,13 +59,6 @@ public void cargarPortatil(String [] parts)
    String correo = parts[3];
     String nombre = parts[4];
     String visibilidad = parts[5];
-    
-    /*if(parts[1].equals("") & parts[2].equals(""))
-    {
-       correo = parts[3];
-       nombre = parts[4];
-       visibilidad = parts[5];
-    }*/
     ManejoComputadoraPortatil.getInstancia().cargarPortatil(correo, nombre, visibilidad);
 }
 
@@ -77,8 +69,7 @@ public void cargarPhone(String[] parts)
     String correo = parts[3];
     String nombre = parts[4];
     String visible = parts[5];
-    ManejoSmartPhone.getInstancia().cargarPhone( numero, correo, nombre, visible);
-    //smartphone,5555-0001,,correo01@correo.com,s_01,si				 
+    ManejoSmartPhone.getInstancia().cargarPhone( numero, correo, nombre, visible);		 
 }
 
 public void cargarTablet(String[] parts)
@@ -86,7 +77,6 @@ public void cargarTablet(String[] parts)
     String correo = parts[3];
     String nombre = parts[4];
     String visibilidad = parts[5];
-    //tablet,,,correo02@correo.com,t_02,si	
     ManejoTablet.getInstancia().cargarTablet(nombre, correo, visibilidad);
 }
 
